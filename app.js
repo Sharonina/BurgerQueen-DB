2d57256f70eabb9cf3dfc0a4c6f90b2bce66ab83
Sharon Arana <shar_mdza@hotmail.com>
2023-02-17 10:17:43 -0600

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("dotenv").config(); //expone las variables de entorno con process.env
 const express = require("express"); //framework de node para bknd
 const { errorHandler } = require("./middleware/errorHandler");
+const cors = require("cors");
 
 require("./config/database").connect();
 const routerApi = require("./router");
 
 const app = express(); //instancia de express en app
+app.use(cors());
 app.use(express.json()); //middleware: transforma el json que mandamos para poder leerlo
 routerApi(app);
 app.use(errorHandler);
