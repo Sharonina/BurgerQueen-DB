042179b64bdd1f09e64ffc2fd0d3a65a0405a816
Sharonina <shar_mdza@hotmail.com>
2023-02-21 19:48:28 -0600

    add change to error middleware response

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 function errorHandler(error, req, res, next) {
-  res.status(error.statusCode || 500).send({ message: error.message });
+  res.status(error.statusCode || 500).send({ error: error.message });
 }
 
 module.exports = { errorHandler };
